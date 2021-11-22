d7624b2c707f317e08e57867e9559e6e1255c170
Lucas Cordeiro <lucas.cordeiro@rarolabs.com.br>
2021-11-22 18:27:45 -0300

    Configura port

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,6 @@ async function bootstrap() {
     entities: [Users]
   })
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
